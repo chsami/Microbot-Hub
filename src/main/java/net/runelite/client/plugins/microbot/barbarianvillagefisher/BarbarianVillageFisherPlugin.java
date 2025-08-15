e21aa404ec669ae2389ab7924b148b9b60ae8755
StickToTheScript <nyancatmaker@gmail.com>
2025-08-15 11:32:43 -0500

    Add Varrock Anvil and Barbarian Village Fisher

---
+++ src/main/java/net/runelite/client/plugins/microbot/barbarianvillagefisher/BarbarianVillageFisherPlugin.java
@@ -0,0 +1,61 @@
+package net.runelite.client.plugins.microbot.barbarianvillagefisher;
+
+import com.google.inject.Provides;
+import lombok.extern.slf4j.Slf4j;
+import net.runelite.api.Client;
+import net.runelite.client.Notifier;
+import net.runelite.client.callback.ClientThread;
+import net.runelite.client.config.ConfigManager;
+import net.runelite.client.plugins.Plugin;
+import net.runelite.client.plugins.PluginDescriptor;
+import net.runelite.client.plugins.microbot.Microbot;
+import net.runelite.client.plugins.microbot.util.mouse.VirtualMouse;
+import net.runelite.client.ui.overlay.OverlayManager;
+
+import javax.inject.Inject;
+import java.awt.*;
+
+@PluginDescriptor(
+	name = PluginDescriptor.StickToTheScript + "Barb Village Fisher",
+	author = "StickToTheScript",
+	version = "1.0.0",
+	description = "Fly and bait fish in Barbarian Village.",
+	tags = {"fish", "fishing", "fly", "bait", "barb", "barbarian", "village", "sticktothescript", "stts"},
+	iconUrl = "https://oldschool.runescape.wiki/images/thumb/Barbarian_Village_%282007%29.png/591px-Barbarian_Village_%282007%29.png?27aac",
+	enabledByDefault = false,
+	isExternal = true,
+	minClientVersion = "1.9.6"
+)
+
+@Slf4j
+public class BarbarianVillageFisherPlugin extends Plugin {
+    @Inject
+    private BarbarianVillageFisherConfig config;
+    @Inject
+    private OverlayManager overlayManager;
+    @Inject
+    private BarbarianVillageFisherOverlay overlay;
+
+    @Provides
+    BarbarianVillageFisherConfig provideConfig(ConfigManager configManager) {
+        return configManager.getConfig(BarbarianVillageFisherConfig.class);
+    }
+
+    @Inject
+    BarbarianVillageFisherScript script;
+
+
+    @Override
+    protected void startUp() throws AWTException {
+        if (overlayManager != null) {
+            overlayManager.add(overlay);
+        }
+
+        script.run(config);
+    }
+
+    protected void shutDown() {
+        script.shutdown();
+        overlayManager.remove(overlay);
+    }
+}
