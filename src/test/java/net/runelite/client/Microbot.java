a0010b3c6b71290d665e9ead581a25bcd9e882c8
g-mason0 <19415334+g-mason0@users.noreply.github.com>
2025-08-15 15:44:51 -0400

    fix(microbot): use concrete collector

--- src/test/java/net/runelite/client/Microbot.java
+++ src/test/java/net/runelite/client/Microbot.java
@@ -2,6 +2,7 @@ package net.runelite.client;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import net.runelite.client.plugins.microbot.pestcontrol.PestControlPlugin;
 
 public class Microbot
@@ -13,7 +14,7 @@ public class Microbot
 
     public static void main(String[] args) throws Exception
     {
-		List<Class<?>> _debugPlugins = Arrays.stream(debugPlugins).toList();
+		List<Class<?>> _debugPlugins = Arrays.stream(debugPlugins).collect(Collectors.toList());
         RuneLiteDebug.pluginsToDebug.addAll(_debugPlugins);
         RuneLiteDebug.main(args);
     }
