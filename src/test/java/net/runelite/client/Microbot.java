3ad075cfa41a3975758c395afb05a5f31581ad59
Your GitHub Username <your.email@example.com>
2025-08-27 22:13:38 +0100

    feat: Add Jewellery Enchant and Bones to Bananas plugins

--- src/test/java/net/runelite/client/Microbot.java
+++ src/test/java/net/runelite/client/Microbot.java
@@ -3,14 +3,16 @@ package net.runelite.client;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-import net.runelite.client.plugins.microbot.example.ExamplePlugin;
+import net.runelite.client.plugins.microbot.bonestobananas.BonesToBananasPlugin;
+import net.runelite.client.plugins.microbot.jewelleryenchant.JewelleryEnchantPlugin;;
 
 public class Microbot
 {
 
-	private static final Class<?>[] debugPlugins = {
-		ExamplePlugin.class
-	};
+    private static final Class<?>[] debugPlugins = {
+            JewelleryEnchantPlugin.class,
+            BonesToBananasPlugin.class
+    };
 
     public static void main(String[] args) throws Exception
     {
