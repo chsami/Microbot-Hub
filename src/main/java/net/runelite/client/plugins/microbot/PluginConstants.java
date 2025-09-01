2d754f40a8999150cc6656abb9a81b90a80c41d6
heap-overfl0w <heapoverfl0w@pm.me>
2025-09-01 18:32:36 -0500

    added UIM Charter Crafter plugin; not only for UIMs, just UIM-friendly and no banking required

--- src/main/java/net/runelite/client/plugins/microbot/PluginConstants.java
+++ src/main/java/net/runelite/client/plugins/microbot/PluginConstants.java
@@ -25,6 +25,7 @@ public final class PluginConstants
 	public static final String BGA = "<html>[<font color=#FF6B35>bga</font>] ";
   public static final String Lumusi = "<html>[<font color=#FFD700><b>\uD83E\uDD16</b></font>] ";
   public static final String BASCHE = "<html>[<font color=#07A6F0>B</font>] ";
+    public static final String heapoverfl0w = "<html>[<font color=#07A6F0>h</font>] ";
 
   public static final boolean DEFAULT_ENABLED = false;
   public static final boolean IS_EXTERNAL = true;
