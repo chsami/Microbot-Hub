3d97b92f9b3173a829b6442326643a299f925e6d
heap-overfl0w <heapoverfl0w@pm.me>
2025-09-01 23:25:08 -0500

    added a config section on how to contact me and the requirements before starting the script

--- src/main/java/net/runelite/client/plugins/microbot/chartercrafter/CharterCrafterConfig.java
+++ src/main/java/net/runelite/client/plugins/microbot/chartercrafter/CharterCrafterConfig.java
@@ -3,9 +3,33 @@ package net.runelite.client.plugins.microbot.chartercrafter;
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
+import net.runelite.client.config.ConfigSection;
+import net.runelite.client.config.ConfigInformation;
 
 @ConfigGroup("chartercrafter")
+@ConfigInformation(
+        "<html>" +
+        "<div style='margin:0; padding:0;'>If you have any questions, please contact <b>heapoverfl0w</b> in the <b>Microbot</b> Discord!</div>" +
+        "<br/>" +
+        "<b>Requirements:</b>" +
+        "<ul style='margin:0; padding-left: 10px; list-style-position: inside;'>" +
+        "<li>Glassblowing pipe in inventory</li>" +
+        "<li>Coins ≥ 1,000</li>" +
+        "<li>Astral runes ≥ 2</li>" +
+        "<li>Either Air runes ≥ 10 or wear Staff of air / Air battlestaff</li>" +
+        "<li>Either Fire runes ≥ 6 or wear Staff of fire / Fire battlestaff</li>" +
+        "<li>Be near a Trader Crewmember (no walking logic is included)</li>" +
+        "</ul>" +
+        "</html>"
+)
 public interface CharterCrafterConfig extends Config {
+
+    @ConfigSection(
+            name = "Settings",
+            description = "Charter Crafter settings",
+            position = 0
+    )
+    String SETTINGS_SECTION = "settings";
 
     enum Product {
         BEER_GLASS("Beer glass", "Beer glass"),
@@ -28,7 +52,9 @@ public interface CharterCrafterConfig extends Config {
     @ConfigItem(
             keyName = "product",
             name = "Product",
-            description = "Glass item to make"
+            description = "Glass item to make",
+            section = SETTINGS_SECTION,
+            position = 0
     )
     default Product product() {
         return Product.BEER_GLASS;
