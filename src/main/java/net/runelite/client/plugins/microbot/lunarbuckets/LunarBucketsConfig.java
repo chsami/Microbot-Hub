c8706b507c663bd1f9303018bba84587ae361efc
Netoxic <ernesto.reza.torres@gmail.com>
2025-09-02 19:14:10 -0600

    Description.

--- src/main/java/net/runelite/client/plugins/microbot/lunarbuckets/LunarBucketsConfig.java
+++ src/main/java/net/runelite/client/plugins/microbot/lunarbuckets/LunarBucketsConfig.java
@@ -6,9 +6,9 @@ import net.runelite.client.config.ConfigInformation;
 
 @ConfigGroup("lunarbuckets")
 @ConfigInformation(
-        "\u2022 Automatically casts Humidify to fill buckets.\\n" +
-        "\u2022 Ensure your bank contains a steam staff, astral runes and empty buckets.\\n" +
-        "\u2022 No configuration is required."
+		"▪ Automatically casts Humidify to fill buckets.<br /><br />" +
+			"▪ Ensure your bank contains a steam staff, astral runes and empty buckets.<br /><br />" +
+			"▪ Ensure you are on Lunar Spellbook"
 )
 public interface LunarBucketsConfig extends Config {
 }
