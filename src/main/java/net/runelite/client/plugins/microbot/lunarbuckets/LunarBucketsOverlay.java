e05f6983678c1c8389beb33caeef945379a66ea6
Netoxic <ernesto.reza.torres@gmail.com>
2025-09-03 00:07:11 -0600

    Better overlay size.

--- src/main/java/net/runelite/client/plugins/microbot/lunarbuckets/LunarBucketsOverlay.java
+++ src/main/java/net/runelite/client/plugins/microbot/lunarbuckets/LunarBucketsOverlay.java
@@ -22,24 +22,38 @@ public class LunarBucketsOverlay extends OverlayPanel {
 
     @Override
     public Dimension render(Graphics2D graphics) {
+        panelComponent.getChildren().clear();
+
+        String title = "Lunar Buckets v" + LunarBucketsPlugin.version;
+        String statusRight = Microbot.status;
+        String profitRight = Integer.toString(plugin.getTotalProfit());
+        String pphRight = Integer.toString(plugin.getProfitPerHour());
+
+        FontMetrics fm = graphics.getFontMetrics();
+        int width = fm.stringWidth(title);
+        width = Math.max(width, fm.stringWidth("Status:" + statusRight));
+        width = Math.max(width, fm.stringWidth("Profit:" + profitRight));
+        width = Math.max(width, fm.stringWidth("Profit/h:" + pphRight));
+        panelComponent.setPreferredSize(new Dimension(width + 20, 0));
+
         panelComponent.getChildren().add(TitleComponent.builder()
-                .text("Lunar Buckets v" + LunarBucketsPlugin.version)
+                .text(title)
                 .color(Color.CYAN)
                 .build());
 
         panelComponent.getChildren().add(LineComponent.builder()
                 .left("Status:")
-                .right(Microbot.status)
+                .right(statusRight)
                 .build());
 
         panelComponent.getChildren().add(LineComponent.builder()
                 .left("Profit:")
-                .right(Integer.toString(plugin.getTotalProfit()))
+                .right(profitRight)
                 .build());
 
         panelComponent.getChildren().add(LineComponent.builder()
                 .left("Profit/h:")
-                .right(Integer.toString(plugin.getProfitPerHour()))
+                .right(pphRight)
                 .build());
 
         return super.render(graphics);
