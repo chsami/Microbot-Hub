b9128121044dd0af318990b7ac256e5089f66e86
Netoxic <ernesto.reza.torres@gmail.com>
2025-09-26 21:34:28 -0600

    Fix imports.

--- src/main/java/net/runelite/client/plugins/microbot/PlayerMonitor/FlashOverlay.java
+++ src/main/java/net/runelite/client/plugins/microbot/PlayerMonitor/FlashOverlay.java
@@ -1,4 +1,4 @@
-package net.runelite.client.plugins.microbot.storm.plugins.PlayerMonitor;
+package net.runelite.client.plugins.microbot.plugins.PlayerMonitor;
 
 import java.awt.*;
 
--- src/main/java/net/runelite/client/plugins/microbot/PlayerMonitor/PlayerMonitorScript.java
+++ src/main/java/net/runelite/client/plugins/microbot/PlayerMonitor/PlayerMonitorScript.java
@@ -1,4 +1,4 @@
-package net.runelite.client.plugins.microbot.storm.plugins.PlayerMonitor;
+package net.runelite.client.plugins.microbot.plugins.PlayerMonitor;
 
 import net.runelite.api.Varbits;
 import net.runelite.api.coords.WorldPoint;
