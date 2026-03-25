d61ed143d90a0e5057ec01f547ce33887a62ecd2
Jonathan Thomas <95548936+JThomasDevs@users.noreply.github.com>
2026-03-24 21:54:53 -0600

    re-add microbot.java. whoops.

---
+++ src/test/java/net/runelite/client/Microbot.java
@@ -0,0 +1,22 @@
+package net.runelite.client;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import net.runelite.client.plugins.microbot.slayer.SlayerPlugin;
+
+public class Microbot
+{
+
+	private static final Class<?>[] debugPlugins = {
+            SlayerPlugin.class,
+	};
+
+    public static void main(String[] args) throws Exception
+    {
+		List<Class<?>> _debugPlugins = Arrays.stream(debugPlugins).collect(Collectors.toList());
+        RuneLiteDebug.pluginsToDebug.addAll(_debugPlugins);
+        RuneLiteDebug.main(args);
+    }
+}
